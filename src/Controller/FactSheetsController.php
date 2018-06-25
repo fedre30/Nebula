45244e55815c86cfbc88c2ea637a1137a2294316
CorentinCrz <ccroiz@live.fr>
2018-06-25 11:59:27 +0200

    api POST, form and http reponse

--- src/Controller/FactSheetsController.php
+++ src/Controller/FactSheetsController.php
@@ -2,11 +2,14 @@
 namespace App\Controller;
 
 
+use App\Form\FactSheetsType;
 use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations as FOSRest;
 use App\Entity\FactSheets;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Brand controller.
@@ -17,16 +20,16 @@ class FactSheetsController extends FOSRestController
 {
     /**
      * Lists all Articles.
-     * @FOSRest\Get("/fact")
+     * @FOSRest\Get("/facts")
      */
     public function getMovies()
     {
         $repository = $this->getDoctrine()->getRepository(FactSheets::class);
 
         // query for a single Product by its primary key (usually "id")
-        $movies = $repository->findAll();
+        $FactSheets = $repository->findAll();
 
-        $view = View::create($movies);
+        $view = View::create($FactSheets);
         $view->setFormat('json');
 
         // Gestion de la réponse
@@ -34,17 +37,43 @@ class FactSheetsController extends FOSRestController
     }
 
     /**
-     * Lists one Article.
-     * @Route("/fact/{id}", name="fact_id", methods={"GET"}, requirements={"id"="\d+"})
+     * one Article.
+     * @Route("/facts/{id}", name="facts_id", methods={"GET"}, requirements={"id"="\d+"})
      */
     public function getOneMovie($id)
     {
         $repository = $this->getDoctrine()->getRepository(FactSheets::class);
-        // query for a single Product by its primary key (usually "id")
-        $movies = $repository->find($id);
-        $view = View::create($movies);
+        $FactSheets = $repository->find($id);
+
+        if ($FactSheets === null) {
+            $view = View::create($FactSheets, Response::HTTP_NOT_FOUND , []);
+        } else {
+            $view = View::create($FactSheets, Response::HTTP_OK , []);
+        }
         $view->setFormat('json');
-        // Gestion de la réponse
+        return $this->handleView($view);
+    }
+
+    /**
+     * Create Article.
+     * @FOSRest\Post("/facts")
+     *
+     */
+    public function PostMoviesAction(Request $request)
+    {
+        $FactSheets = new FactSheets();
+        $form = $this->createForm(FactSheetsType::class, $FactSheets);
+        $form->submit($request->request->all());
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($FactSheets);
+            $em->flush();
+            $view = View::create($FactSheets, Response::HTTP_CREATED , []);
+        } else {
+            $view = View::create($form, Response::HTTP_BAD_REQUEST , []);
+        }
+        $view->setFormat('json');
         return $this->handleView($view);
     }
 }
--- src/Controller/HomeQuestionController.php
+++ src/Controller/HomeQuestionController.php
@@ -2,11 +2,14 @@
 namespace App\Controller;
 
 
+use App\Form\HomeQuestionsType;
 use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\Controller\Annotations as FOSRest;
 use App\Entity\HomeQuestions;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Brand controller.
@@ -33,5 +36,44 @@ class HomeQuestionController extends FOSRestController
         return $this->handleView($view);
     }
 
+    /**
+     * one Article.
+     * @Route("/homeQuestions/{id}", name="facts_id", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function getOneMovie($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(HomeQuestions::class);
+        $HomeQuestions = $repository->find($id);
 
+        if ($HomeQuestions === null) {
+            $view = View::create($HomeQuestions, Response::HTTP_NOT_FOUND , []);
+        } else {
+            $view = View::create($HomeQuestions, Response::HTTP_OK , []);
+        }
+        $view->setFormat('json');
+        return $this->handleView($view);
+    }
+
+    /**
+     * Create Article.
+     * @FOSRest\Post("/homeQuestions")
+     *
+     */
+    public function PostMoviesAction(Request $request)
+    {
+        $HomeQuestions = new HomeQuestions();
+        $form = $this->createForm(HomeQuestionsType::class, $HomeQuestions);
+        $form->submit($request->request->all());
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($HomeQuestions);
+            $em->flush();
+            $view = View::create($HomeQuestions, Response::HTTP_CREATED , []);
+        } else {
+            $view = View::create($form, Response::HTTP_BAD_REQUEST , []);
+        }
+        $view->setFormat('json');
+        return $this->handleView($view);
+    }
 }
