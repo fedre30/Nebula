0f6e14926577d4b50b93f16a9947a34c6bc12706
Federica Alfano <alfanofederica95@gmail.com>
2018-06-27 16:17:14 +0200

    desktop question quiz done

--- assets/js/pages/Quiz_question.vue
+++ assets/js/pages/Quiz_question.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="Quiz">
-        <nebu-menu></nebu-menu>
-        <div class="Quiz-container">
-            <h3 class="Quiz-title">quizz</h3>
-        <h4 class="Quiz-question">un trou noir se forme suite à: </h4>
-            <form method="post">
-                <div class="Form-radio"><p-radio class="p-round p-default Quiz-radio" v-for="(answer, index) in answers" name="radio">{{answer}}</p-radio></div>
-                <router-link :to="{name:'answer'}"><input class="Quiz-button" type="submit" value="valider"/></router-link>
-            </form>
+        <h1 class="Nebula-title desktop">Nebula</h1>
+        <div class="Quiz-background"></div>
+            <nebu-menu></nebu-menu>
+            <div class="Quiz-container">
+                <h3 class="Quiz-title">quizz</h3>
+                <div class="Quiz-questionContainer">
+                    <h4 class="Quiz-question">Un trou noir se forme suite à: </h4>
+                    <form method="post">
+                        <div class="Form-radio">
+                            <p-radio class="p-round p-default Quiz-radio" v-for="(answer, index) in answers"
+                                     name="radio">{{answer}}
+                            </p-radio>
+                            <div class="kurk desktop"></div>
+                        </div>
+                        <router-link :to="{name:'answer'}"><input class="Quiz-button" type="submit" value="valider"/>
+                        </router-link>
+                    </form>
+                </div>
+            </div>
+            <nebu-footer></nebu-footer>
         </div>
-        <nebu-footer></nebu-footer>
-    </div>
 </template>
 
 <script>
@@ -21,9 +31,10 @@
     export default {
         components: {
             nebuMenu,
-            nebuFooter},
+            nebuFooter
+        },
         name: 'quiz-question',
-        data: function() {
+        data: function () {
             return {
                 answers: [
                     'L’explosion d’un astéroïde',
@@ -57,7 +68,7 @@
         text-align: left
         margin: 2rem 0 6rem 0
         text-transform: uppercase
-    &-subtitle
+    &-question
         width: 100%
         font-size: 1.5rem
         text-transform: uppercase
@@ -85,11 +96,104 @@
         color: white
         margin-bottom: 3rem
         text-align: left
+    .desktop
+        display: none
 
+@media(min-width: 600px)
+    .Quiz
+        font-family: Roboto, sans-serif
+        position: relative
+        width: 95vw
+        height: 100vh
+        background: none
+        padding: 3rem
+        margin: 0 auto
+        overflow: hidden
 
+        &-background
+            width: 100%
+            height: 50vh
+            position: absolute
+            bottom: 15vh
+            right: 5vw
+            background: url("../assets/img/quiz_background_desktop.png") no-repeat
+            background-size: 100%
+            box-sizing: border-box
 
+        &-container
+            width: 100vw
+            height: 30vh
+            margin: 0 auto
+            text-align: center
+            padding: 6rem
+        &-title
+            font-size: 2.5rem
+            font-family: Airship, sans-serif
+            color: #231189
+            text-align: left
+            margin: 2rem 0 6rem 0
+            text-transform: uppercase
+            position: absolute
+            transform: rotate(90deg)
+            right: 90px
+            top: 230px
+        &-question
+            font-size: 1.5rem
+            color: #FE5B3F
+            font-style: italic
+            font-weight: 900
+            margin-bottom: 1rem
+            text-transform: none
+        &-button
+            width: 150px
+            height: 50px
+            font-size: 1rem
+            line-height: 50px
+            background: white
+            font-weight: 700
+            border-radius: 10px
+            border: transparent
+            text-transform: uppercase
+            text-align: center
+            margin: 5vh 1vw
+            color: #1F0D87
+            box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1)
+            left: 3rem
+            position: absolute
 
-
+        &-radio
+            width: 100%
+            font-size: 1rem
+            color: black
+            margin: 1.5rem 0
+            text-align: left
+        .desktop
+            display: block
+        .Nebula-title
+            font-size: 3rem
+            position: absolute
+            left: 6rem
+            top: 3rem
+            font-family: Airship, sans-serif
+            color: #F33A56
+        &-questionContainer
+            width: 70vw
+            height: 35vh
+            border-width: 15px
+            border-style: solid
+            border-image: linear-gradient(348deg, rgba(205, 193, 225, 1) 0%, rgba(243, 58, 86, 1) 100%)
+            border-image-slice: 1
+            padding: 4rem
+            position: relative
+            margin-top: 10vh
+        .kurk
+            width: 350px
+            height: 300px
+            position: absolute
+            bottom: 0
+            right: 50px
+            background: url("../assets/img/kurk.png") no-repeat
+            background-size: cover
 
 
 </style>
