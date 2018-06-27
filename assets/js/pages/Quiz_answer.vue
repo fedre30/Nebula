a4bac5b548b5925b55e52f025185ea34913ee252
Federica Alfano <alfanofederica95@gmail.com>
2018-06-27 17:06:55 +0200

    quiz desktop done

--- assets/js/pages/Quiz_answer.vue
+++ assets/js/pages/Quiz_answer.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="Answer">
+        <h1 class="Nebula-title desktop">Nebula</h1>
+        <div class="Answer-background"></div>
         <nebu-menu></nebu-menu>
         <div class="Answer-container">
         <h3 class="Answer-title">quizz</h3>
-            <div class="Answer-question">un trou noir se forme suite à:</div>
+            <div class="Answer-answerContainer">
+            <div class="Answer-question">Un trou noir se forme suite à:</div>
             <div class="Answer-item" v-for="(answer, index) in answers" :key="answer.name" :class="answer.styleClass">{{answer.name}}</div>
             <p class="Answer-text">Mais vous n’avez rien compris !
                 Vous devriez relire la fiche qui en parle si vous voulez mon avis.</p>
-            <router-link :to="{name: 'results'}"><div class="Answer-button">suivant</div></router-link>
+                <router-link :to="{name: 'results'}"><div class="Answer-button">suivant</div></router-link>
+            </div>
+
         </div>
         <nebu-footer></nebu-footer>
     </div>
@@ -138,6 +143,133 @@
 
 a
     text-decoration: none
+
+@media(min-width: 600px)
+    .Answer
+        font-family: Roboto, sans-serif
+        position: relative
+        width: 95vw
+        height: 100vh
+        background: none
+        padding: 3rem
+        margin: 0 auto
+        overflow: hidden
+
+        &-background
+            width: 100%
+            height: 50vh
+            position: absolute
+            bottom: 15vh
+            right: 5vw
+            background: url("../assets/img/quiz_background_desktop.png") no-repeat
+            background-size: 100%
+            box-sizing: border-box
+
+        &-container
+            width: 100vw
+            height: 30vh
+            margin: 0 auto
+            text-align: center
+            padding: 6rem
+        &-title
+            font-size: 2.5rem
+            font-family: Airship, sans-serif
+            color: #231189
+            text-align: left
+            margin: 2rem 0 6rem 0
+            text-transform: uppercase
+            position: absolute
+            transform: rotate(90deg)
+            right: 90px
+            top: 230px
+        &-question
+            font-size: 1.5rem
+            color: #FE5B3F
+            font-style: italic
+            font-weight: 900
+            margin-bottom: 1rem
+            text-transform: none
+        &-text
+            font-size: 1rem
+            font-family: Roboto, sans-serif
+            color: black
+            text-align: left
+            line-height: 2rem
+            font-weight: 300
+            font-style: italic
+            margin-top: 2rem
+        &-item
+            width: 50%
+            height: 50px
+            font-size: 1rem
+            font-weight: 700
+            color: black
+            margin-top: 0.5rem
+            line-height: 50px
+
+        &-button
+            width: 150px
+            height: 50px
+            font-size: 1rem
+            line-height: 50px
+            background: white
+            font-weight: 700
+            border-radius: 10px
+            border: transparent
+            text-transform: uppercase
+            text-align: center
+            margin: 5vh 1vw
+            color: #1F0D87
+            box-shadow: 0 2px 4px 0 rgba(0,0,0,0.1)
+            right: 3rem
+            position: absolute
+        &-answerContainer
+            width: 70vw
+            height: 35vh
+            border-width: 15px
+            border-style: solid
+            border-image: linear-gradient(348deg, rgba(205, 193, 225, 1) 0%, rgba(243, 58, 86, 1) 100%)
+            border-image-slice: 1
+            padding: 4rem
+            position: relative
+            margin-top: 10vh
+        .Nebula-title
+            font-size: 3rem
+            position: absolute
+            left: 6rem
+            top: 3rem
+            font-family: Airship, sans-serif
+            color: #F33A56
+.wrong
+    height: 50px
+    background-color: #CDC1E1
+    color: white
+    text-align: left
+    &:before
+        display: inline-block
+        content: ""
+        background: url("../assets/img/cross_desktop.svg") no-repeat
+        width: 20px
+        height: 20px
+        background-size: cover
+        margin: 0 1rem
+        line-height: 50px
+
+
+.right
+    background-color: #F13455
+    color: white
+    text-align: left
+    &:before
+        display: inline-block
+        content: ""
+        background: url("../assets/img/tick_desktop.svg") no-repeat
+        width: 20px
+        height: 20px
+        background-size: cover
+        margin: 0 1rem
+        line-height: 50px
+
 
 
 </style>
